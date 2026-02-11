b8e0dd6f9a17f4395ba4d38821ed56597b36cde7
Yuto Gushiken <yuyuyu11301213@gmail.com>
2026-02-11 19:30:19 +0900

    test: auto deploy check

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -115,7 +115,7 @@ export default function Footer() {
             "
           >
             <InstaIcon />
-            Instagram
+            ヨリソイ : Instagram
           </a>
         </div>
       </div>
--- src/sections_sp/FooterSP.jsx
+++ src/sections_sp/FooterSP.jsx
@@ -131,7 +131,7 @@ export default function FooterSP() {
             "
           >
             <InstaIcon />
-            Instagram
+            ヨリソイ : Instagram
           </a>
         </div>
       </div>
